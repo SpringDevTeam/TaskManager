c7754e5387528102a8d1d68150ddb18bbc93bfc5
Mert <mertdemirok80@gmail.com>
2021-06-11 07:11:57 +0200

    implement UserService

--- src/main/java/be/intecbrussel/taskmanager/service/impl/UserServiceImpl.java
+++ src/main/java/be/intecbrussel/taskmanager/service/impl/UserServiceImpl.java
@@ -1,12 +1,41 @@
 package be.intecbrussel.taskmanager.service.impl;
 
-import be.intecbrussel.taskmanager.model.JobTitle;
 import be.intecbrussel.taskmanager.model.entity.UserEntity;
+import be.intecbrussel.taskmanager.repository.UserRepository;
 import be.intecbrussel.taskmanager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.Collection;
 
-public class UserServiceImpl  {
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+
+    @Override
+    public UserEntity getUserById(Long id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public UserEntity saveUser(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    @Override
+    public UserEntity updateUser(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    @Override
+    public void deleteUser(UserEntity userEntity) {
+        userRepository.deleteById(userEntity.getId());
+    }
+
+    @Override
+    public Collection<UserEntity> getAllUserEntities() {
+        return userRepository.findAll();
+    }
 }
